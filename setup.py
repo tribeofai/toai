cc7290910d111a2867e65e4b771c8a4d6471076e
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-07-29 10:33:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-from setuptools import setup
-from setuptools import find_packages
+import os
+from ast import literal_eval
+
+from setuptools import find_packages, setup
+
+
+def get_version(version_tuple):
+    if not isinstance(version_tuple[-1], int):
+        return ".".join(map(str, version_tuple[:-1])) + version_tuple[-1]
+    return ".".join(map(str, version_tuple))
+
+
+init = os.path.join(os.path.dirname(__file__), "toai", "__init__.py")
+
+version_line = list(filter(lambda l: l.startswith("VERSION"), open(init)))[0]
+
+# VERSION is a tuple so we need to eval 'version_line'.
+# We could simply import it from the package but we
+# cannot be sure that this package is importable before
+# installation is done.
+PKG_VERSION = get_version(literal_eval(version_line.split("=")[-1]))
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="toai-mokahaiku",
-    version="0.0.1",
+    version=PKG_VERSION,
     description="To AI helper library",
     long_description=long_description,
     long_description_content_type="text/markdown",
