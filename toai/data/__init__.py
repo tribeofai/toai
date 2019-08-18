305e3db3b53a875c3e8e9181d39ca90faf5a484b
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-08-18 15:15:53 +0300

    Add split_df data util

--- toai/data/__init__.py
+++ toai/data/__init__.py
@@ -1,3 +1,4 @@
 # pylama:ignore=W0611
 
 from .DataParams import DataParams
+from .utils import split_df
---
+++ toai/data/utils.py
@@ -0,0 +1,14 @@
+def split_df(data, test_size, target_col=None, random_state=42):
+    if target_col:
+        stratify = data[target_col]
+    train_data, test_data = train_test_split(
+        data, test_size=test_size, stratify=stratify, random_state=random_state
+    )
+    if target_col:
+        test_stratify = test_data[target_col]
+    val_data, test_data = train_test_split(
+        test_data, test_size=0.5, stratify=test_stratify, random_state=random_state
+    )
+    for df in train_data, val_data, test_data:
+        df.reset_index(drop=True, inplace=True)
+    return train_data, val_data, test_data
