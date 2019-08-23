c98c82a063e7168f6dce573b5d62e6bd3be03013
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-08-23 17:57:23 +0300

    Update imports

--- toai/__init__.py
+++ toai/__init__.py
@@ -1,2 +1,2 @@
-VERSION = (0, 0, 28)
+VERSION = (0, 0, 29)
 __version__ = ".".join([str(x) for x in VERSION])
--- toai/data/__init__.py
+++ toai/data/__init__.py
@@ -1,4 +1,5 @@
 # pylama:ignore=W0611
 
 from .DataParams import DataParams
+from .DataContainer import DataContainer
 from .utils import split_df
