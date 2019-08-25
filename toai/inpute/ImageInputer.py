ae2fc2298122b8fa0213386f5b0bb22ce50002d8
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-08-25 22:57:57 +0300

    Add ImageInputer.py

---
+++ toai/inpute/ImageInputer.py
@@ -0,0 +1,28 @@
+import os
+from typing import List, Optional
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class ImageInputer(BaseEstimator, TransformerMixin):
+    def __init__(self, value, columns: Optional[List[str]] = None):
+        self.value = value
+        self.columns = columns
+
+    def fit(self, data: pd.DataFrame) -> "ImageInputer":
+        return self
+
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        def fill_missing_with_value(value):
+            def inner(x):
+                return x if os.path.isfile(x) else value
+
+            return inner
+
+        data = data.copy()
+        for column in self.columns or data.columns:
+            data[column] = (
+                data[column].apply(fill_missing_with_value(self.value)).values
+            )
+        return data
