dd9fc96db9ebd61ff0e38a535068b1813f7c0f2f
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-08-31 21:19:27 +0300

    Update split_df data util

--- toai/data/utils.py
+++ toai/data/utils.py
@@ -1,14 +1,17 @@
+from typing import Tuple
+
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def split_df(data, test_size, target_col=None, random_state=42):
-    if target_col:
-        stratify = data[target_col]
+def split_df(
+    data: pd.DataFrame, test_size: float, target_col: str = None, random_state: int = 42
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    stratify = data[target_col] if target_col else None
     train_data, test_data = train_test_split(
         data, test_size=test_size, stratify=stratify, random_state=random_state
     )
-    if target_col:
-        test_stratify = test_data[target_col]
+    test_stratify = test_data[target_col] if target_col else None
     val_data, test_data = train_test_split(
         test_data, test_size=0.5, stratify=test_stratify, random_state=random_state
     )
