b99781ca3c2bb4e5a09c2eb46ce617719a33dfad
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-09-29 18:44:09 +0300

    Fix linter comment

--- toai/utils/load_file.py
+++ toai/utils/load_file.py
@@ -3,4 +3,4 @@ import pickle  # nosec
 
 def load_file(filename, mode="rb"):
     with open(filename, mode=mode) as f:
-        return pickle.load(f)  # nosecV
+        return pickle.load(f)  # nosec
