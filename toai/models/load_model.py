b52e94bc5e37f7a344a6ac69c74d0732692c7b2c
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2019-11-18 16:58:55 +0200

    Update load_model module

--- toai/models/load_model.py
+++ toai/models/load_model.py
@@ -1,12 +1,14 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, Optional
 from tensorflow import keras
 
 
 def load_keras_model(
-    architecture_path: Union[Path, str], weights_path: Union[Path, str]
+    architecture_path: Union[Path, str],
+    weights_path: Union[Path, str],
+    custom_objects: Optional[dict] = None,
 ):
     with open(architecture_path, "r") as f:
-        model = keras.models.model_from_json(f.read())
+        model = keras.models.model_from_json(f.read(), custom_objects=custom_objects)
     model.load_weights(weights_path)
     return model
