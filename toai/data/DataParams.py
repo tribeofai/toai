1ba500b29c4327e378f6acbefd0ad2fed31a4d9f
Dovydas Ceilutka <10526751+mokahaiku@users.noreply.github.com>
2020-05-23 07:33:12 +0300

    Refactor DataParams

--- toai/data/DataParams.py
+++ toai/data/DataParams.py
@@ -1,20 +1,17 @@
-from typing import List, Optional
+from typing import List
+import attr
 
 
+@attr.s(auto_attribs=True)
 class DataParams:
-    def __init__(
-        self,
-        target_col: str,
-        cat_cols: Optional[List[str]] = None,
-        cont_cols: Optional[List[str]] = None,
-        text_cols: Optional[List[str]] = None,
-        img_cols: Optional[List[str]] = None,
-    ):
-        self.target_col = target_col
-        self.cat_cols = cat_cols or []
-        self.cont_cols = cont_cols or []
-        self.text_cols = text_cols or []
-        self.img_cols = img_cols or []
+    target_col: str
+    cat_cols: List[str] = attr.Factory(list)
+    cont_cols: List[str] = attr.Factory(list)
+    text_cols: List[str] = attr.Factory(list)
+    img_cols: List[str] = attr.Factory(list)
+    feature_cols: List[str] = attr.ib(init=False)
+
+    def __attrs_post_init__(self):
         self.feature_cols = (
             self.cat_cols + self.cont_cols + self.text_cols + self.img_cols
         )
